1906a480ace5dbfad39b5462833b5a7db69adbd8
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2019-03-25 00:21:04 +0700

    [Dev] Get and showing data from backend

--- assets/js/components.js
+++ assets/js/components.js
@@ -45,29 +45,57 @@ Vue.component('app-header', {
 Vue.component('app-main', {
   data: () => ({
     tambahitem,
+    contacts: [],
+    name: '',
+    phone: '',
   }),
+  async mounted () {
+    this.contacts = (await ajax.get('/')).data;
+  },
+  methods: {
+    simpan() {
+      window.alert(this.name + this.phone);
+    }
+  },
   template: `<main class="columns is-centered" style="margin-top: 10px">
     <div class="column is-6">
 
       <!-- TAMBAH KONTAK -->
-      <div class="columns" v-if="tambahitem.show">
+      <div class="columns" v-if="tambahitem.show" style="margin-bottom: 25px">
         <div class="column card">
           <div class="card-header">
             <div class="card-header-title">Tambah Kontak</div>
           </div>
           <div class="card-content">
-            Nama: <br/> Nomor:
+            <label class="label">Nama
+              <input class="input" type="text" v-model="name" placeholder="Masukkan Nama">
+            </label>
+            <label class="label">Telepon
+              <input class="input" type="text" v-model="phone" placeholder="Masukkan Nomor Telepon">
+            </label>
           </div>
           <div class="card-footer">
-            <a class="card-footer-item" style="color: #3273dc">Simpan</a>
-            <a class="card-footer-item" style="color: red" @click="tambahitem.show = false">Batal</a>
+            <a class="card-footer-item" style="color: #3273dc" @click="simpan">Simpan</a>
+            <a class="card-footer-item" style="color: red" @click="tambahitem.show = false; name = ''; phone = ''">Batal</a>
           </div>
         </div>
       </div>
 
       <!-- LIST KONTAK -->
       <div class="columns">
-          <div class="column">list kontak</div>
+          <div class="column card">
+            <div class="card-header">
+              <div class="card-header-title">List Kontak</div>
+            </div>
+            <div class="card-content">
+              <div class="list is-hoverable">
+                <span class="list-item" v-for="(kontak, index) in contacts" :key="index">
+                  <span>{{kontak.name}}</span>
+                  <span style="color: #999">- {{kontak.phone}}</span>
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
     </div>
   </main>`,
